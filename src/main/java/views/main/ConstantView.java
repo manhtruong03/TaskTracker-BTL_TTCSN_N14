609ad2417089599e6bf7e43fe294fabc06664480
manhtruong03 <manhtruong03.it@gmail.com>
2023-12-26 00:40:37 +0700

    Delete redundant files

--- src/main/java/views/main/ConstantView.java
+++
@@ -1,9 +0,0 @@
-package main.java.views.main;
-
-import java.awt.*;
-
-public class ConstantView {
-	public static final Dimension DEFAULT_WINDOW_SIZE = new Dimension(1280, 720);
-	public static final int WINDOW_WIDTH = 1280;
-	public static final int WINDOW_HEIGHT = 720;
-}
